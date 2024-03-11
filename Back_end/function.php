131156d4d0a00f057d0d672f5821c9ef651405b6
Blaise <blaiseaganzepascal10@gmail.com>
2024-03-11 07:48:23 -0700

    Create Insert project function in function.php

--- Back_end/function.php
+++ Back_end/function.php
@@ -1,2 +1,17 @@
 <?php 
 // create Crud du produit
+require 'BDD.php';
+function insert_products($nom_product,$descriptions,$prix,$quantite_stock){
+    global $pdo;
+    // Préparez la requête d'insertion
+    $requete = $pdo->prepare("INSERT INTO produit (nom_product, descriptions,prix,quantite_stock) VALUES (:nom_product, :descriptions,:prix,:quantite_stock)");
+
+    // Liez les valeurs aux paramètres de la requête
+    $requete->bindParam(':nom_product',$nom_product );
+    $requete->bindParam(':descriptions', $descriptions);
+    $requete->bindParam(':prix', $prix);
+    $requete->bindParam(':quantite_stock', $quantite_stock);
+
+    // Exécutez la requête
+    $requete->execute();
+}
